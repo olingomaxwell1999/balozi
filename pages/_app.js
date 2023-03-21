dfa9c30aeee1d6c0d94a464bdf6d1f8f4d634f1b
Olingo Maxwell <olingomaxwell1999@gmail.com>
2023-03-21 14:22:57 +0300

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { pageview } from "../lib/ga";
 
 import Head from 'next/head'
 
@@ -6,10 +9,46 @@ import Layout from '../components/Layout'
 
 function MyApp ({Component, pageProps}) {
 
+   const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+
+      
+    
       <Head>
-        
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+            });
+            `,
+            }}
+          />
       </Head>
       <Layout>
         <Component {...pageProps}/>
