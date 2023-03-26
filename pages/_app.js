bb77f8a7e3bf191aee4ded20ed2aeb3c173d3973
Olingo Maxwell <olingomaxwell1999@gmail.com>
2023-03-26 03:28:21 +0300

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,12 @@
 import '../styles/globals.css'
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import { pageview } from "../lib/ga";
 
 import Head from 'next/head'
 
 import Layout from '../components/Layout'
+import Script from 'next/script'
 
 function MyApp ({Component, pageProps}) {
 
-   const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      pageview(url);
-    };
-
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <>
@@ -33,24 +14,19 @@ function MyApp ({Component, pageProps}) {
       
     
       <Head>
-        <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+        
+      </Head>
+      <Layout>
+        <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-ZTSB4BN10X'/>
+
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-            });
-            `,
-            }}
-          />
-      </Head>
-      <Layout>
+            gtag('config', 'G-ZTSB4BN10X')
+          `}
+        </Script>
         <Component {...pageProps}/>
       </Layout>
     </>
