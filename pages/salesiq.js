8b7e8136cae9a9b061413f8a3bfcd6d4fe5445b1
olingomaxwell1999 <olingomaxwell1999@gmail.com>
2024-01-23 11:24:25 +0300

    zoho update

--- pages/salesiq.js
+++ pages/salesiq.js
@@ -1,25 +1,30 @@
+// components/ZohoSalesIQ.js
+
 import { useEffect } from "react";
 
 const ZohoSalesIQ = () => {
   useEffect(() => {
     // Paste your Zoho SalesIQ widget code here
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.id = "zsiqscript";
-    script.defer = true;
-    script.src = "https://salesiq.zohopublic.com/widget";
-
-    const existingScript = document.getElementById("zsiqscript");
-    if (existingScript) {
-      existingScript.remove();
-    }
-
-    document.body.appendChild(script);
+    var $zoho = $zoho || {};
+    $zoho.salesiq = $zoho.salesiq || {
+      widgetcode:
+        "siq7dadedad7e9232f65f7edbdf62dd18b68d5c5b32ff2bf0dc7363898d65f29eb4fa9085690e717b75cdc9f364ef853d37",
+      values: {},
+      ready: function () {},
+    };
+    var d = document;
+    var s = d.createElement("script");
+    s.type = "text/javascript";
+    s.id = "zsiqscript";
+    s.defer = true;
+    s.src = "https://salesiq.zohopublic.com/widget";
+    var t = d.getElementsByTagName("script")[0];
+    t.parentNode.insertBefore(s, t);
 
     return () => {
       // Cleanup if the component is unmounted
-      if (script.parentNode) {
-        script.parentNode.removeChild(script);
+      if (s.parentNode) {
+        s.parentNode.removeChild(s);
       }
     };
   }, []);
